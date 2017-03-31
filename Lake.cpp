78756f79ce69fce4182046fdfdd879232632c733
Keno0 <norby185@gmail.com>
2017-03-31 15:24:44 +0100

    remove some stuffs

--- Lake.cpp
+++ Lake.cpp
@@ -48,7 +48,6 @@ class Graph
 	list<AdjListNode> *adj;
 
 	// A function used by longestPath
-	void topologicalSortUtil(int v, bool visited[], stack<int> &Stack);
 	void printAllPathsUtil(int, int, bool[], int[], int &);
 public:
 	Graph(int V); // Constructor
@@ -57,7 +56,6 @@ public:
 	void addEdge(int u, int v, int weight);
 
 	// Finds longest distances from given source vertex
-	void longestPath(int s);
 	void printAllPaths(int s, int d);
 
 	void PrintBestPathEndDistance();
@@ -95,71 +93,7 @@ void Graph::addEdge(int u, int v, int weight)
 	adj[u].push_back(node); // Add v to u’s list
 }
 
-// A recursive function used by longestPath. See below link for details
-// http://www.geeksforgeeks.org/topological-sorting/
-void Graph::topologicalSortUtil(int v, bool visited[], stack<int> &Stack)
-{
-	// Mark the current node as visited
-	visited[v] = true;
 
-	// Recur for all the vertices adjacent to this vertex
-	list<AdjListNode>::iterator i;
-	for (i = adj[v].begin(); i != adj[v].end(); ++i)
-	{
-		AdjListNode node = *i;
-		if (!visited[node.getV()])
-			topologicalSortUtil(node.getV(), visited, Stack);
-	}
-
-	// Push current vertex to stack which stores topological sort
-	Stack.push(v);
-}
-
-// The function to find longest distances from a given vertex. It uses
-// recursive topologicalSortUtil() to get topological sorting.
-void Graph::longestPath(int s)
-{
-	stack<int> Stack;
-	int *dist = new int[V];
-
-	// Mark all the vertices as not visited
-	bool *visited = new bool[V];
-	for (int i = 0; i < V; i++)
-		visited[i] = false;
-
-	// Call the recursive helper function to store Topological Sort
-	// starting from all vertices one by one
-	for (int i = 0; i < V; i++)
-		if (visited[i] == false)
-			topologicalSortUtil(i, visited, Stack);
-
-	// Initialize distances to all vertices as infinite and distance
-	// to source as 0
-	for (int i = 0; i < V; i++)
-		dist[i] = NINF;
-	dist[s] = 0;
-
-	// Process vertices in topological order
-	while (Stack.empty() == false)
-	{
-		// Get the next vertex from topological order
-		int u = Stack.top();
-		Stack.pop();
-
-		// Update distances of all adjacent vertices
-		list<AdjListNode>::iterator i;
-		if (dist[u] != NINF)
-		{
-			for (i = adj[u].begin(); i != adj[u].end(); ++i)
-				if (dist[i->getV()] < dist[u] + i->getWeight())
-					dist[i->getV()] = dist[u] + i->getWeight();
-		}
-	}
-
-	// Print the calculated longest distances
-	for (int i = 0; i < V; i++)
-		(dist[i] == NINF) ? cout << "INF " : cout << dist[i] << " ";
-}
 
 // Prints all paths from 's' to 'd'
 void Graph::printAllPaths(int s, int d)
@@ -566,21 +500,10 @@ int main()
 	vertices++;
 	Graph g(vertices);
 	g.ReadDatasFromConsole();
-	//g.addEdge(0, 1, 30);
-	//g.addEdge(1, 2, 15);
-	//g.addEdge(2, 3, 60);
-	//g.addEdge(3, 4, 45);
-	//g.addEdge(4, 5, 60);
-	//g.addEdge(2, 4, 30);
 
-	//int s = 1;
-	//cout << "Following are longest distances from source vertex " << s << " \n";
-	//g.longestPath(s);
-	int s = 0, d = vertices-1;
-	//cout << "Following are all different paths from " << s	<< " to " << d << endl;
+	int s = 0, d = vertices-1;	
 	g.printAllPaths(s, d);
 
-	//g.PrintBestPathEndDistance();
 	g.CheckNumberOfCompTravel();
 
 	return 0;
